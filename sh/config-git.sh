9a308ae3f757c6d152f8be8eaa913c5fd3b2842f
kq <ykqyao@qq.com>
2018-05-28 07:35:02 +0800

    config

---
+++ sh/config-git.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cp ~/.bashrc ../my_bashrc
+cp ~/.config/i3/config ../my_i3
+cp ~/.config/i3status/config ../my_i3status
+cp ~/.vimrc ../my_vimrc 
---
+++ sh/git-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp my_bashrc ~/.bashrc
+cp my_i3 ~/.i3/config
+cp my_vimrc ~/.vimrc 
+
