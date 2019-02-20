05727a52e32e0964a5639d5c7764c3d8bf4beecb
falconkaze <ykqyao@qq.com>
2019-02-20 21:45:49 +0800

    guake, bashrc

--- sh/autostart/guake.sh
+++ sh/autostart/guake.sh
@@ -2,20 +2,11 @@
 #!/usr/bin/env bash
 
 #guake &
-guake -r git
+
 # 下面这两个命令不能合并为一行，如果合在一起会导致两条命令都在同一层shell中执行，得不到预期的结果。涉及到切换shell的命令（比如tmux就是创建一个子shell），不要把命令合并在一起。同理，要使用shell脚本切换当前目录，必须使用source命令，因为默认情况下，shell脚本是在当前shell的子shell中执行的，影响不到当前目录。
-guake -e "tmux new -s git"
-guake -e backend
-guake -e 'tmux split-window -h'
-guake -e jv
-
-guake -n 1 -r db -e "tmux new -s db"
-
-guake -n 1 -r redis -e 'tmux new -s redis'
-
 guake -n 1 -r ydcv -e "tmux new -s ydcv"
 guake -e "tmux split-window -h;ydcv"
-guake -e ". note.sh;vim + english.md"
+guake -e "note;vim + english.md"
 
 guake -n 1 -r note -e "tmux new -s note"
-guake -e '. note.sh'
+guake -e 'note'
