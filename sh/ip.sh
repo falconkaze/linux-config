a33e17322764a1c2f3ca3e4b148f67a535febda2
falconkaze <ykqyao@qq.com>
2019-09-25 22:48:11 +0800

    [alias] get ip

--- sh/ip.sh
+++ sh/ip.sh
@@ -3,7 +3,10 @@
 
 # 获取电脑ip
 
-net_dev=`ls /sys/class/net | grep -E '^(eth|wlan|enp|wlp)'`
-ip=`ifconfig ${net_dev} | grep "inet " | awk '{print $2}'`
-
-echo ${ip}
+net_devs=`ls /sys/class/net | grep -E '^(eth|wlan|enp|wlp)'`
+for net_dev in $net_devs
+do
+  echo -n "${net_dev}: "
+  ip=`ifconfig ${net_dev} | grep "inet " | awk '{print $2}'`
+  echo ${ip}
+done
